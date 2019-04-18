fd7da609dd32fe4064039eb0ff63b2878ce4a100
Rick Reilly <rreilly@edx.org>
2019-04-18 16:49:53 -0400

    wip

--- lms/djangoapps/program_enrollments/api/v1/urls.py
+++ lms/djangoapps/program_enrollments/api/v1/urls.py
@@ -9,7 +9,7 @@ app_name = 'lms.djangoapps.program_enrollments'
 
 urlpatterns = [
     url(
-        r'^programs/{program_key}/enrollments/$'.format(program_key=r'.*'),
+        r'^programs/{program_key}/enrollments/$'.format(program_key=r'(?P<program_key>[0-9a-fA-F-]+)'),
         ProgramEnrollmentsView.as_view(),
         name='course_grades'
     ),
--- lms/djangoapps/program_enrollments/api/v1/views.py
+++ lms/djangoapps/program_enrollments/api/v1/views.py
@@ -4,9 +4,12 @@ ProgramEnrollment Views
 """
 from __future__ import unicode_literals
 
+import logging
+
 from django.http import HttpResponse
 from rest_framework.views import APIView
 
+LOGGER = logging.getLogger(__name__)
 
 class ProgramEnrollmentsView(APIView):
     """
@@ -14,4 +17,5 @@ class ProgramEnrollmentsView(APIView):
     """
 
     def get(self, request, *args, **kwargs):
+        LOGGER.info('Rickie is the coolest ever')
         return HttpResponse('result')
