12e45f4e4eae4a1da6255a8a208f7a6755785a5a
Rick Reilly <rreilly@edx.org>
2019-04-22 15:46:42 -0400

    wip

--- lms/djangoapps/program_enrollments/api/v1/views.py
+++ lms/djangoapps/program_enrollments/api/v1/views.py
@@ -9,7 +9,6 @@ import logging
 from django.http import HttpResponse
 from rest_framework.views import APIView
 
-LOGGER = logging.getLogger(__name__)
 
 class ProgramEnrollmentsView(APIView):
     """
@@ -17,5 +16,5 @@ class ProgramEnrollmentsView(APIView):
     """
 
     def get(self, request, *args, **kwargs):
-        LOGGER.info('Rickie is the coolest ever')
+        from pdb import set_trace;
         return HttpResponse('result')
